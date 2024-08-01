b5139066c9a4ae31f5c5d9a3f97be41f83808689
Joe Sixpack <lokeshkumaetelagamalla@g_mail.com>
2024-08-02 01:22:02 +0530

    seventh

--- src/components/Wishes.js
+++ src/components/Wishes.js
@@ -4,7 +4,7 @@ import "./styles.css"; // Ensure you have this CSS file in the src directory
 
 const Wishes = () => {
   useEffect(() => {
-    emailjs.init("segsdhdsh"); // Replace with your EmailJS user ID
+    emailjs.init("TemSz5cC3VMXtzamM"); // Replace with your EmailJS user ID
 
     const sendNotification = () => {
       emailjs
